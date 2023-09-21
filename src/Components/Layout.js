7e3c19e30686dee0b19e5a79577b33ed263293ff
omar shalaby <97606456+OmarShalaby9@users.noreply.github.com>
2023-09-21 13:33:18 +0300

    init

---
+++ src/Components/Layout.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../CSS/Layout.css";
+
+
+function Layout() {
+  return (
+    <div className="layout">
+      <div className="overlay">
+        <div className="Content">
+          <div className="text">
+            <p>
+              <h1>Omar Shalaby</h1>
+            </p>
+            <p className="web">
+              <h4>Full-Stack web</h4>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
